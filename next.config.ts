0e7784479169599e327e96c79cf6f6be38516506
SpynitterMina <Spynittergames@gmail.com>
2025-05-09 02:01:06 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -18,6 +19,7 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  output: 'export',
 };
 
 export default nextConfig;
